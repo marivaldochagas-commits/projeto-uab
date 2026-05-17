d43f4019b3ceb88f1c6b7677b9a4570a44f1cd5f
Marivaldo Lopes <marivaldo.chagas@estudante.ifto.edu.br>
2026-05-16 21:36:07 -0300

    Correção de segurança: Código do login organizado com a SECRET_KEY

--- run.py
+++ run.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# CONFIGURAÇÃO DA CHAVE SECRETA (Correção do Item 3)
+app.config['SECRET_KEY'] = 'uma_chave_criptografica_muito_segura_e_longa_123'
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect('/dashboard') # Ao clicar em entrar, vai para o relatório
+        return redirect('/dashboard')  # Ao clicar em entrar, vai para o relatório
     return render_template('login.html')
 
 @app.route('/cadastro')
